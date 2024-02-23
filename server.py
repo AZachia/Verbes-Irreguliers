d2e7a363e4c5c339c5addef8e117f2c6b646727f
AZachia <154826103+AZachia@users.noreply.github.com>
2024-02-23 08:48:53 +0100

    update server.py

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+from pycsv import csv
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
-    return "Hello World"
+    return render_template("index.html")
+
+
 
 
 if __name__ == "__main__":
