49c51ae5f3fdf3e286ba637a8dd35f556f33902c
AZachia <154826103+AZachia@users.noreply.github.com>
2024-02-23 09:40:53 +0100

    update server.py

--- server.py
+++ server.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template, jsonify, request, session
+from flask import Flask, render_template, jsonify, request, session, redirect, url_for
 from pycsv import csv
 import os
 
 app = Flask(__name__)
 
 # enumere tous les fichiers csv dans /verbes pour permettre a l'utilisateur de choisir son niveau
-fichiers_verbes = []
+fichiers_verbes = {}
 for verbe_file in os.listdir("verbes"):
     print(verbe_file)
-    fichiers_verbes.append(verbe_file)
+    fichiers_verbes[verbe_file.replace(".csv", "")] = csv().load_file("verbes/" + verbe_file)
     
     
 ### Fonctions Internes au programme
-
 
 @app.route("/internal/select-file")
 def select_file():
@@ -23,7 +22,17 @@ def select_file():
 
 ### Fonctions publques
 
+@app.route("/run/<verbesfile>")
+def run(verbesfile):
+    """Lance le programme avec le fichier / niveau choisisss"""
+    if verbesfile in fichiers_verbes:
+        return "c'est bon: " + verbesfile
+    return redirect("/")
+
+
+
 @app.route("/")
+@app.route("/home")
 def index():
     return render_template("index.html")
 
