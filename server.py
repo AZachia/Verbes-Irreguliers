06d3a2854096be15a7773a13a5f3d25d370f6d39
AZachia <154826103+AZachia@users.noreply.github.com>
2024-02-23 08:55:44 +0100

    add csv file handle

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 from flask import Flask, render_template
 from pycsv import csv
+import os
 
 app = Flask(__name__)
 
+
+
+
+# enumere tous les fichiers csv dans /verbes pour permettre a l'utilisateur de choisir son niveau
+fichiers_verbes = []
+for verbe_file in os.listdir("verbes"):
+    print(verbe_file)
+    fichiers_verbes.append(verbe_file)
 
 
 @app.route("/")
