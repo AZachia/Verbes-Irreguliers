d63343514e056cf36d19b598d2ae0390bf8e832c
AZachia <154826103+AZachia@users.noreply.github.com>
2024-02-24 10:54:35 +0100

    update js

--- static/index.js
+++ static/index.js
@@ -5,3 +5,24 @@ function xor_crypt(message, cle) {
     }
     return message_code;
 }
+
+
+
+function start() {
+    for (let i = 0; i < verbes.length; i++) {
+        verbe = xor_crypt(verbes[i][0], key);
+        verbeElement.innerHTML = verbe;
+    }
+    
+}
+
+
+const startBtn = document.getElementById("start-btn");
+const verbeElement = document.getElementById("verbe-element");
+const sendBnt = document.getElementById("send-btn");
+
+const bvElement = document.getElementById("bv-element");
+const preteritElement = document.getElementById("preterit-element");
+const ppElement = document.getElementById("pp-element");
+
+startBtn.addEventListener("click", start);
